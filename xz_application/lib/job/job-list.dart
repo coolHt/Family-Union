eb39ed47d44897940ccc5af3977f8f609436d40a
handsomeBoy <>
2019-09-26 01:14:44 +0800

    修改joblist布局

--- xz_application/lib/job/job-list.dart
+++ xz_application/lib/job/job-list.dart
@@ -28,72 +28,90 @@ Card jobList(Map data){
       margin: EdgeInsets.fromLTRB(10.0, 0, 10.0, 15.0),
       child: Padding(
         padding: EdgeInsets.all(12.0),
-        child: Column(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(bottom: 12.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Expanded(
+              flex: 3,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(
-                    data['jobName'],
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0
-                    )
-                  ),
-                  Text(
-                    data['salary'],
-                    style:TextStyle(fontSize: 15.0)
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 12.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    '宁波 | 5-7年经验 | 大专',
-                    style: TextStyle(
-                      fontSize: 15.0
-                    )
-                  ),
-                  Text(
-                    data['date'],
-                    style:TextStyle(fontSize: 15.0)
-                  ),
-                ],
-              )
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      data['companyName'],
+                  Container(
+                    margin: EdgeInsets.only(bottom: 8.0),
+                    child: Text(
+                      data['jobName'],
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
                       style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0
+                      )
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 8.0),
+                    child: Text(
+                      '宁波 | 10年以上经验 | 大专',
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 15.0
+                      )
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 8.0),
+                    child: Text(
+                      data['companyName'],
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 15.0,
                         fontWeight: FontWeight.bold
                       )
                     ),
-                    Row(
-                      children: <Widget>[
-                        Text('外贸(非欧美) | '),
-                        Text('50-150人 | '),
-                        Text('汽车及零配件'),
-                      ],
+                  ),
+                  Text(
+                    '外资(欧美) | 10000人以上 | 汽车及零配件',
+                    softWrap: false,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 15.0,
                     )
-                  ]
-                ),
-                Image(
-                  width: 40.0,
-                  image: AssetImage('images/job.jpg'),
-                )
-              ]
-            )
+                  )
+                ],
+              )
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(bottom: 8.0),
+                    child: Text(
+                      data['salary'],
+                      style: TextStyle(
+                        fontSize: 15.0
+                      )
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 8.0),
+                    child: Text(
+                      data['date'],
+                      style: TextStyle(
+                        fontSize: 15.0
+                      )
+                    ),
+                  ),
+                  Image(
+                    width: 50.0,
+                    image: AssetImage('images/job.jpg'),
+                  )
+                ],
+              )
+            ),
           ],
         )
       )
