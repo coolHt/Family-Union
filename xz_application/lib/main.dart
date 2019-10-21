cb207dd11190f2e8b982bda941cb096f6cbf6525
handsomeboyHT <>
2019-10-21 21:07:21 +0800

    update main

--- xz_application/lib/main.dart
+++ xz_application/lib/main.dart
@@ -1,22 +1,12 @@
 import 'package:flutter/material.dart';
 //网络调试
 import 'package:flutter_flipperkit/flutter_flipperkit.dart';
-import 'dart:convert';
-//路由列表
-//import './route-list.dart' as R;
 //职位列表
 import './job/job-list.dart' as Jobs;
 //企业列表
 import './company/company-list.dart' as Companys;
 //职位详情
 import './job/job-detail.dart' as JobDetail;
-//
-import 'package:dio/dio.dart';
-import 'package:dio/adapter.dart';
-//外部服务器代理
-import 'package:shelf/shelf_io.dart' as shelf_io;
-import 'package:shelf_proxy/shelf_proxy.dart';
-
 void main(){
   FlipperClient flipperClient = FlipperClient.getDefault();
   
@@ -31,7 +21,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //proxyAddress();
     return MaterialApp(
       initialRoute: '/',
       routes: {
@@ -47,25 +36,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-void proxyAddress() async{
-  //设置外部代理
-  var server = await shelf_io.serve(
-    proxyHandler('http://122.112.143.38:8004'),
-    '127.0.0.1',
-    8333
-  );
-  print(server.address);
-  print('Proxying at http://${server.address.host}:${server.port}');
-  try{
-    Dio dio = new Dio();
-    String jsonStr = '{"action":"reg", "type":"p","mobil":"15825568423"}';
-    Map<String, dynamic> obj = json.decode(jsonStr);
-    Response response = await dio.post("http://122.112.143.38:8004/Psninfo", data:obj);
-    print(response.data.toString());
-  }catch(e){
-    print(e);
-  }
-}
-
 
