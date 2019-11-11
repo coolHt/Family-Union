ec72341c008e52d524f1078b484014598f1227e0
handsomeboyHT <ht2631503150@gmail.com>
2019-11-12 00:43:31 +0800

    update

--- xz_application/lib/job/job-list.dart
+++ xz_application/lib/job/job-list.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+
 import 'package:dio/dio.dart';
 
 import 'package:flutter/material.dart';
--- xz_application/lib/login.dart
+++ xz_application/lib/login.dart
@@ -1,24 +1,48 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
 
-class LoginContainer extends StatelessWidget{
+class LoginContainer extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     double deviceWidth = MediaQuery.of(context).size.width;
     double deviceHeight = MediaQuery.of(context).size.height;
+    final _fromKey = GlobalKey < FormState > ();
     return Scaffold(
       body: Container(
         width: deviceWidth,
         height: deviceHeight,
         decoration: BoxDecoration(
           image: DecorationImage(
-            image:ExactAssetImage('images/loginBg.jpg'),
-            fit:BoxFit.fitHeight
+            image: ExactAssetImage('images/loginBg.jpg'),
+            fit: BoxFit.fitHeight
           )
         ),
         child: Column(
-          children: <Widget>[
-            Image.asset('images/insLogo.png')
+          children: < Widget > [
+            Image.asset('images/insLogo.png'),
+            Container(
+              width: 200.0,
+              child: Form(
+                //key: _fromKey,
+                child: Column(
+                  children: < Widget > [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "hello, there is input",
+                        labelStyle: TextStyle(
+                          color: Colors.amberAccent,
+                          fontWeight: FontWeight.bold
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4.0)), //这么变态
+
+                        )
+                      ),
+
+                    )
+                  ],
+                )
+              )
+            )
           ],
         )
       )
