78f9feebf42df215d55d163107c5437ec9007b34
handsomeboyHT <ht2631503150@gmail.com>
2019-11-25 23:37:34 +0800

    countNumber

--- fu_application/lib/message-code.dart
+++ fu_application/lib/message-code.dart
@@ -1,45 +1,78 @@
 import 'package:flutter/material.dart';
-import './components/fu-input.dart' as FormInput;
-import './commonMethod.dart' as Methods;
+import 'dart:async';
+import './components/fu-input.dart'
+as FormInput;
+import './commonMethod.dart'
+as Methods;
 
- class MessageCode extends StatefulWidget{
-   MessageCode({Key key}) : super(key: key);
-   @override
-   MessageCodeState createState() => MessageCodeState();
- }
+class MessageCode extends StatefulWidget {
+  MessageCode({
+    Key key
+  }): super(key: key);
+  @override
+  MessageCodeState createState() => MessageCodeState();
+}
 
- class MessageCodeState extends State<MessageCode>{
+class MessageCodeState extends State < MessageCode > {
+  //监听输入
+  final _controller = TextEditingController();
+  //判断手机号码格式
+  bool verityPhone = false;
+  //倒计时
+  num countNum = 60;
+  //是否显示倒计时
+  bool showNum = false;
+  @override
+  void initState() {
+    _controller.addListener(() {
+      if (_controller.text != "") {
+        bool res = Methods.identifyPhoneType(_controller.text);
+        if (res) {
+          //验证通过
+          this.setState(() {
+            verityPhone = true;
+          });
 
-   //监听输入
-   final _controller = TextEditingController();
-   //判断手机号码格式
-   bool verityPhone = false;
-   @override
-   void initState(){
-     _controller.addListener((){
-       if(_controller.text != ""){
-          bool res = Methods.identifyPhoneType(_controller.text);
-          if(res){
-            this.setState((){
-              verityPhone = true;
-            });
-          }else{
-            this.setState((){
-              verityPhone = false;
-            });
-          }
-       }
-     });
-    super.initState();
-   }
-  
-   @override
-   Widget build(BuildContext context){
-     double deviceWidth = MediaQuery.of(context).size.width;
-     double deviceHeight = MediaQuery.of(context).size.height;
-     double wExtent = deviceWidth * 0.8;
-     
-     Map contentPadding = { 
+        } else {
+          //验证失败
+          this.setState(() {
+            verityPhone = false;
+          });
+        }
+      }
+    });
+  }
+
+  //倒计时
+  void countDown() {
+    const timeout =
+      const Duration(seconds: 1);
+    new Timer.periodic(
+      timeout,
+      (Timer t) {
+        countNum -= 1;
+        this.setState(() {
+          countNum = countNum;
+        });
+        if (countNum == 1) {
+          t.cancel();
+        }
+      }
+    );
+  }
+
+  //发送验证码
+  postCode() {
+    countDown();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double deviceWidth = MediaQuery.of(context).size.width;
+    double deviceHeight = MediaQuery.of(context).size.height;
+    double wExtent = deviceWidth * 0.8;
+
+    Map contentPadding = {
       "left": 0.0,
       "top": 15.0,
       "right": 0.0,
@@ -51,16 +84,31 @@ import './commonMethod.dart' as Methods;
       fit: BoxFit.fitWidth,
       image: AssetImage("images/phone.png"),
     );
-    //发送验证码
-    postCode(){
+
+    //倒计时widget
+    countNumber(){
+      if(showNum){
+        return Container(
+          width: wExtent,
+          child: Text(
+            "$countNum秒后可重新获取",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold
+            )
+          ),
+        );
+      }
       
     }
-     return Scaffold(
-       body: GestureDetector(
-         onTap: (){
-           FocusScope.of(context).requestFocus(new FocusNode());
-         },
-         child: Container(
+
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(new FocusNode());
+        },
+        child: Container(
           width: deviceWidth,
           height: deviceHeight,
           decoration: BoxDecoration(
@@ -70,62 +118,64 @@ import './commonMethod.dart' as Methods;
             )
           ),
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.white,
-                        width: 2.0
-                      )
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: < Widget > [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.white,
+                      width: 2.0
                     )
-                  ),
-                  margin: EdgeInsets.only(bottom: 20.0),
-                  width: wExtent,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Expanded(
-                        flex:3,
-                        child: FormInput.fuInput(
-                          controllerFun: _controller,
-                          hintText: "请输入手机号",
-                          borderColor: Color.fromRGBO(255, 255, 255, 0), 
-                          focusBorderColor: Color.fromRGBO(255, 255, 255, 0),
-                          fontSize: 16.0, 
-                          textColor: Colors.white,
-                          contentPadding: contentPadding,
-                          keyboardType: "number",
-                          iconAssets: iconStyle
-                        )
-                      ),
-                       //获取验证码
-                      Expanded(
-                        child:OutlineButton(
-                          //验证码发送状态
-                          onPressed: verityPhone ? postCode : null,
-                          disabledBorderColor: Colors.transparent,
-                          padding: EdgeInsets.all(0),
-                          borderSide: BorderSide(
-                            color: Colors.transparent
-                          ),
-                          child: Text(
-                            "获取验证码",
-                            style: TextStyle(
-                              fontSize: 16.0
-                            )
-                          ),
-                          textColor: Colors.white,
-                        ),
-                      )
-                    ],
                   )
                 ),
-              ],
-            )
+                margin: EdgeInsets.only(bottom: 10.0),
+                width: wExtent,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: < Widget > [
+                    Expanded(
+                      flex: 3,
+                      child: FormInput.fuInput(
+                        controllerFun: _controller,
+                        hintText: "请输入手机号",
+                        borderColor: Color.fromRGBO(255, 255, 255, 0),
+                        focusBorderColor: Color.fromRGBO(255, 255, 255, 0),
+                        fontSize: 16.0,
+                        textColor: Colors.white,
+                        contentPadding: contentPadding,
+                        keyboardType: "number",
+                        iconAssets: iconStyle
+                      )
+                    ),
+                    //获取验证码
+                    Expanded(
+                      child: OutlineButton(
+                        //验证码发送状态
+                        onPressed: verityPhone ? postCode : null,
+                        disabledBorderColor: Colors.transparent,
+                        padding: EdgeInsets.all(0),
+                        borderSide: BorderSide(
+                          color: Colors.transparent
+                        ),
+                        child: Text(
+                          "获取验证码",
+                          style: TextStyle(
+                            fontSize: 16.0
+                          )
+                        ),
+                        textColor: Colors.white,
+                      ),
+                    )
+                  ],
+                )
+              ),
+              //倒计时
+              countNumber()
+            ],
+          )
         ),
-       )
-     );
-   }
- }
+      )
+    );
+  }
+}
