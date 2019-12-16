c4a66056965e143ea1e9b7733364775353a5c02d
handsomeboyHT <ht2631503150@gmail.com>
2019-12-17 00:19:14 +0800

    Test to use animation

--- fu_application/lib/home.dart
+++ fu_application/lib/home.dart
@@ -1,20 +1,19 @@
 //首页
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'dart:math';
+import 'dart:math' as math;
 
 class MainHome extends StatefulWidget{
   MainHome({
-    Key key
+    Key key,
   }): super(key: key);
   @override
   MainHomeState createState() => MainHomeState();
 }
 
-
-
-class MainHomeState extends State<MainHome>{
-  List bgUrls = <String>["images/au.jpg","images/beauty.jpg","images/farry.jpg","images/house.jpg","images/mountains.jpg","images/rainfost.jpg","images/scenery.jpg"];
+class MainHomeState extends State<MainHome> with SingleTickerProviderStateMixin{
+  AnimationController _controller;
+  List bgUrls = <String>["images/au.jpg","images/beauty.jpg","images/house.jpg","images/mountains.jpg","images/rainfost.jpg","images/scenery.jpg","images/farry.jpg"];
   String randomBg = "";
   @override void initState() {
     //背景随机切换
@@ -23,14 +22,19 @@ class MainHomeState extends State<MainHome>{
     new Timer.periodic(
       Duration(seconds: 5),
       (Timer t){
-        int r = Random().nextInt(randomNum);
+        int r = math.Random().nextInt(randomNum);
         this.setState((){
           randomBg = bgUrls[r];
         });
       }
     );
+    _controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this
+    )..repeat();
     super.initState();
   }
+
   
   @override 
   Widget build(BuildContext context){
@@ -43,6 +47,35 @@ class MainHomeState extends State<MainHome>{
             image: AssetImage(randomBg)
           )
         ),
+        // child: Transform.rotate(
+        //   angle: _controller.value * 2.0,
+        //   child: Container(
+        //     margin: EdgeInsets.fromLTRB(50.0, 50.0, 0, 0),
+        //     decoration: BoxDecoration(
+        //       color: Colors.red
+        //     ),
+        //     width: 50,
+        //     height: 50
+        //   ),
+        // ),
+        child: AnimatedBuilder(
+          animation: _controller,
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            color: Colors.green,
+            child: const Center(
+              child: Text('Wee'),
+            ),
+          ),
+          builder: (BuildContext context, Widget child) {
+            return Transform.rotate(
+              angle: _controller.value * 2.0 * math.pi,
+              child: child,
+            );
+          },
+        )
+        
       )
     );
   }
